cb73af41d7dee2ebe58bf41a4ad582a0aa19a666
ManuGonza <manuelhernangonzalesrojas1999@gmail.com>
2023-08-30 11:45:39 -0500

    español

--- app/src/main/java/com/example/a19200211_20200077_articulo_compose/MainActivity.kt
+++ app/src/main/java/com/example/a19200211_20200077_articulo_compose/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ComponentActivity() {
 @Composable
     fun ArticuloCompose(){
         ArticuloContenido(
-            title= stringResource(R.string.tutorial_jetpack_compose),
+            titulo= stringResource(R.string.tutorial_jetpack_compose),
             pequeniaDescripcion= stringResource(R.string.jetpack_compose_es_un_conjunto_de_herramientas_moderno_para_crear_una_interfaz_de_usuario_nativa_de_android_compose_simplifica_y_acelera_el_desarrollo_de_la_interfaz_de_usuario_en_android_con_menos_c_digo_herramientas_potentes_y_api_de_kotlin_intuitivas),
             largaDescripcion= stringResource(R.string.en_este_tutorial_crear_un_componente_de_interfaz_de_usuario_simple_con_funciones_declarativas_llamas_a_las_funciones_de_compose_para_decir_qu_elementos_quieres_y_el_compilador_de_compose_hace_el_resto_compose_se_basa_en_funciones_componibles_estas_funciones_le_permiten_definir_la_interfaz_de_usuario_de_su_aplicaci_n_mediante_programaci_n_porque_le_permiten_describir_c_mo_deber_a_verse_y_proporcionar_dependencias_de_datos_en_lugar_de_centrarse_en_el_proceso_de_construcci_n_de_la_interfaz_de_usuario_como_inicializar_un_elemento_y_luego_adjuntarlo_a_un_padre_para_crear_una_funci_n_composable_agrega_la_anotaci_n_composable_al_nombre_de_la_funci_n),
             imagePainter = painterResource(R.drawable.bg_compose_background)
@@ -50,7 +50,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
     private fun ArticuloContenido(
-    title:String,
+    titulo:String,
     pequeniaDescripcion:String,
     largaDescripcion:String,
     modifier:Modifier=Modifier,
@@ -59,7 +59,7 @@ class MainActivity : ComponentActivity() {
         Column(modifier=modifier){
             Image(painter = imagePainter, contentDescription = null)
             Text(
-                text=title,
+                text=titulo,
                 modifier=Modifier.padding(16.dp),
                 fontSize=24.sp
             )
